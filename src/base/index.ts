5c014423e42755daca5929971cc565d2dd2111e3
Kazuichi Takashiro <takashiro@qq.com>
2023-02-15 21:40:14 +0800

    Expose classes and functions

---
+++ src/base/index.ts
@@ -0,0 +1,3 @@
+export { default as BatchComparator } from './BatchComparator';
+export { default as Comparator } from './Comparator';
+export { default as TestReport } from './TestReport';
---
+++ src/util/index.ts
@@ -0,0 +1,3 @@
+export { default as glob } from './glob';
+export { default as idle } from './idle';
+export { default as waitFor } from './waitFor';
